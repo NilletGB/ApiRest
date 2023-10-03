272c9206c03a4c1ce2441d6c3059e29632446e70
Gamboa <gamboa.miller@gmail.com>
2023-10-03 15:57:45 -0500

    ApiRest

---
+++ src/main/java/com/example/estructuraapibase/EstructuraApiBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.estructuraapibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstructuraApiBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EstructuraApiBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/estructuraapibase/EstructuraApiBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estructuraapibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstructuraApiBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
